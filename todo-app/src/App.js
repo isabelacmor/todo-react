40e5e458aee0bda511ea34e6a3fb565672d02032
Isabela <isabela.cmor@gmail.com>
2018-05-04 18:27:15 -0700

    removed boilerplate code

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -96,9 +96,6 @@ class App extends Component {
         ) : (
           <span>empty</span>
         )}
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
